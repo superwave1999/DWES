3f7f51caa93417efa4414a6e6ffe6a423f0219d8
superwave1999 <superwave1999@gmail.com>
2018-12-06 03:52:32 +0100

    Delete old version

--- Instasnap/classes/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(
-  function ($clase) {
-    $archivo = dirname(__FILE__) . '/' . str_replace('\\', '/', $clase) . '.php';
-    if (file_exists($archivo)) {
-      require($archivo);
-    }
-});
